ad727d00839ca8bc04df1bead8cbe5b58f210b75
hanawa02 <laura@laura-melo.com>
2023-06-14 15:28:09 +0200

    Add global import of wcl to nuxt 3 library

--- blinkist-content-pages/nuxt.config.ts
+++ blinkist-content-pages/nuxt.config.ts
@@ -1,5 +1,12 @@
-import { defineNuxtConfig } from "nuxt/config"
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   css: ["web-component-library/dist/style.css"]
-})
+
+  imports: {
+    transform: {
+      // https://github.com/nuxt/nuxt/issues/18823#issuecomment-1419704343
+      exclude: [/web-component-library/],
+    },
+  },
+});
--- blinkist-content-pages/plugins/wcl.js
+++ blinkist-content-pages/plugins/wcl.js
@@ -1,7 +1,5 @@
+import WCL from "web-component-library";
 
-import { PersonalButton, BButton } from "web-component-library";
-
-export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.component("PersonalButton", PersonalButton)
-  nuxtApp.vueApp.component("BButton", BButton)
-})
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(WCL);
+});
